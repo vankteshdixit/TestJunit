325886c31f0ba528302cf7be05440b1fedf7063e
vankteshdixit <dixitvanktesh08@gmail.com>
2025-10-24 02:40:01 +0530

    learning testing

---
+++ src/test/java/com/vanktesh/ApplicationTesting/ApplicationTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanktesh.ApplicationTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
